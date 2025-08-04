f05b620734275600b19ce37b5634a135e5e622fa
Paul Tamal <tamal.paul.42123@gmail.com>
2025-08-04 22:52:39 +0600

    Session 1 (introduction) Task 1

--- StringName.java
+++ StringName.java
@@ -4,12 +4,12 @@ public class StringName {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("input length: ");
-        int N = scanner.nextInt();
+
         System.out.println("input sting: ");
-        String S = scanner.next();
-        for(int i = 0; i<N; i++){
-            for (int j = 0; j <N-i; j++){
+
+        String S = scanner.nextLine();
+        for (int i = 0; i < S.length(); i++) {
+            for (int j = 0; j < S.length() - i; j++) {
                 System.out.print(S.charAt(j) + " ");
             }
         }
